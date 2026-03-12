4304fccb138be1e177328ff897d036d916897fa3
Emma <chrillesen04@outlook.dk>
2026-03-12 16:04:47 +0100

    Update script.js

--- exercise/script.js
+++ exercise/script.js
@@ -66,3 +66,69 @@ document.getElementById("todos").addEventListener("click", function (event) {
     event.target.style.textDecoration = "line-through"; // strike the clicked todo item
   }
 });
+// Js - objekt
+// Samling af data
+
+//objekt
+const teacher1 = {
+  email: "jrs@ek.dk",
+  firstname: "Jeppe",
+  lastname: "Stockmar",
+};
+
+const teacher2 = {
+  email: "kirs@ek.dk",
+  firstname: "Christian",
+  lastname: "Kirschberg",
+};
+
+//.                   teacher er et navn som jeg har valgt her
+function printTeacher(teacher) {
+  document.getElementById("pTeacher").textContent =
+    "Hej " +
+    teacher.firstname +
+    " " +
+    teacher.lastname +
+    " din email er " +
+    teacher.email +
+    " hvis du er for gammel til at huske den";
+}
+
+printTeacher(teacher1);
+
+const quiz1 = {
+  // skriv et spørgsmål og et svar. 2 Attributter. question og answer
+  question1: "What is JavaScript?",
+  answer1:
+    "JavaScript is a programming language used to create interactive effects within web browsers.",
+};
+function printQuestion1() {
+  document.getElementById("question1").textContent = quiz1.question1;
+}
+
+document.querySelector("#btnShow").addEventListener("click", function () {
+  document.querySelector("#answer1").classList.toggle("hidden");
+  if (document.querySelector("#answer1").classList.contains("hidden")) {
+    document.querySelector("#btnShow").textContent = "Show answers";
+  } else {
+    document.querySelector("#btnShow").textContent = "Hide answers";
+  }
+});
+const quiz2 = {
+  question2: "What is a variable in JavaScript?",
+  answer2:
+    "A variable in JavaScript is a container for storing data values. It can hold different types of data, such as numbers, strings, or objects.",
+};
+function printQuestion2() {
+  document.getElementById("question2").textContent = quiz2.question2;
+}
+document.querySelector("#btnShow").addEventListener("click", function () {
+  document.querySelector("#answer2").classList.toggle("hidden");
+  if (document.querySelector("#answer2").classList.contains("hidden")) {
+    document.querySelector("#btnShow").textContent = "Show answers";
+  } else {
+    document.querySelector("#btnShow").textContent = "Hide answers";
+  }
+});
+
+
